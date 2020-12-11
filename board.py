6bf794787c699062c9c6d8e21a55ce1e8781a19f
VladMatveenko <74789678+VladiMatveenko@users.noreply.github.com>
2020-12-11 22:43:09 +0300

    Beta board added

--- board.py
+++ board.py
@@ -1,62 +1,67 @@
 import pygame
 
 
-print("hello world")
-class Board:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.board = [[0] * width for _ in range(height)]
-        self.left = 10
-        self.top = 10
-        self.cell_size = 30
+pygame.init()
 
-    def set_view(self, left, top, cell_size):
+class Board:
+    def __init__(self, width, height, num):
+        self.height = height
+        self.width = width
+        self.size = width, height
+        self.num = num
+        #список клеток с персонажами
+        self.list_per = []
+        for i in range(8):
+            b = []
+            for j in range(8):
+                b.append(0)
+            self.list_per.append(b)
+        self.size_k = 50
+        self.top = 50
+        self.left = 50
+
+    def setting(self, left, top, size):
         self.left = left
         self.top = top
-        self.cell_size = cell_size
+        self.size_k = size
 
-    def render(self, surface):
-        wcolor = pygame.Color("white")
-        for i in range(self.height):
-            for j in range(self.width):
-                pygame.draw.rect(surface, wcolor, (self.left + self.cell_size * j, self.top + self.cell_size * i,
-                                                   self.cell_size, self.cell_size), 1 if self.board[i][j] == 0 else 0)
-
-    def get_click(self, mouse_pos):
-        cell_coords = self.get_cell(mouse_pos)
-        if cell_coords is None:
-            return
-
-        self.on_click(cell_coords)
-
-    def get_cell(self, mouse_pos):
-        board_width = self.width * self.cell_size
-        board_height = self.height * self.cell_size
-        if self.left < mouse_pos[0] < self.left + board_width:
-            if self.top < mouse_pos[1] < self.top + board_height:
-                cell_coords = (mouse_pos[1] - self.left) // self.cell_size,\
-                              (mouse_pos[0] - self.top) // self.cell_size
-                return cell_coords
-        return None
-
-    def on_click(self, cell_coords):
-        i = cell_coords[0]
-        j = cell_coords[1]
-        self.board[i][j] = 0 if self.board[i][j] == 1 else 1
-pygame.init()
-size = 1000, 1000
+    def draw(self, color):
+        for i in range(self.num):
+            for j in range(self.num):
+                pygame.draw.rect(screen, pygame.Color(str(color)),
+                                (self.left + self.size_k * j, self.top + self.size_k * i,
+                                 self.size_k, self.size_k), 1)
+    def move_option(self, coord_x, coord_y):
+        x, y = coord_x, coord_y
+        x -= self.left
+        y -= self.top
+        x = x // self.size_k
+        y = y // self.size_k
+        return (x, y)
+    def in_board(self, coord_x, coord_y):
+        if self.left <= coord_x <= self.left + self.size_k * self.num:
+            if self.top <= coord_y <= self.top + self.size_k * self.num:
+                return True
+        return False
+            
+size = 800, 600
 screen = pygame.display.set_mode(size)
-board = Board(8, 8)
-board.set_view(20, 20, 100)
+screen.fill((0, 0, 0))
+a = Board(size[0], size[1], 8)
+a.setting(10, 10, 50)
+a.draw("white")
 running = True
 while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
-    if event.type == pygame.MOUSEBUTTONDOWN:
-      board.get_click(event.pos)
-  screen.fill((0, 0, 0))
-  board.render(screen)
-  pygame.display.flip()
-pygame.quit() 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if pygame.key.key_code("space") == pygame.K_SPACE:
+                print("hello")
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if in_board(event.pos[0], event.pos[1]):
+
+
+    pygame.display.flip()
+
+pygame.quit()
