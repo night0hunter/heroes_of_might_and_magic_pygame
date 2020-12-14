66320708a253aafdf008da3f67fc8f524a09d2c8
VladMatveenko <74789678+VladiMatveenko@users.noreply.github.com>
2020-12-14 22:26:38 +0300

    cursor added

--- board.py
+++ board.py
@@ -1,20 +1,12 @@
 import pygame
 import sqlite3
 import math
-import pygame
-import os
-from cursor import Cursor
 
-con = sqlite3.connect("units.db")
+con = sqlite3.connect("heroes_of_might_and_magic_pygame\\units.db")
 cur = con.cursor()
 pygame.init()
 pygame.display.set_caption("Проект")
-image = load_image("arrow.png")
-image_boom = load_image("arrow.png")
-size = 800, 600
-screen = pygame.display.set_mode(size)
-pygame.mouse.set_visible(False)
-MANUAL_CURSOR = pygame.image.load('arrow.png').convert_alpha()
+
 
 class Board:
     # вводятся значения экрана и значения по умолчанию
@@ -142,7 +134,9 @@ abc = [[1, 0, 0, 0, 0, 0, 1, 0],
        [0, 1, 0, 1, 0, 1, 0, 0],
        [1, 0, 0, 0, 0, 0, 0, 1]]
 
-if __name__ == '__main__':      
+if __name__ == '__main__': 
+    size = 800, 600
+    screen = pygame.display.set_mode(size)    
     screen2 = pygame.Surface(screen.get_size())
     screen.fill((0, 0, 0))
     a = Board(size[0], size[1], 8, 8)
@@ -151,7 +145,6 @@ if __name__ == '__main__':
     FPS = 60
     person = "Костяной дракон"
     running = True
-    draw = False
     a.input_list_per(abc)
     while running:
         screen.blit(screen2, (0, 0))
@@ -168,8 +161,6 @@ if __name__ == '__main__':
                     a.draw_move_option()
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
                 screen2.fill((0, 0, 0))
-            if event.type == pygame.MOUSEMOTION:
-                
         clock.tick(FPS)
         screen.fill((0, 0, 0))
         screen.blit(screen2, (0, 0))
