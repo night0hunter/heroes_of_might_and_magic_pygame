9c9f73aaf83f0a7d29fe7c54f707cbc290535a52
VladMatveenko <74789678+VladiMatveenko@users.noreply.github.com>
2020-12-16 15:23:26 +0300

    cursor added

--- cursor.py
+++ cursor.py
@@ -1,14 +1,53 @@
-class Cursor(pygame.sprite.Sprite):
-    image = load_image("arrow.png")
-    image_arrow = load_image("arrow.png")
+import os
+import sys
+import pygame
+import random
 
-    def __init__(self, group):
-        super().__init__(group)
-        self.image = Cursor.image
-        self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(width)
-        self.rect.y = random.randrange(height)
+# Изображение не получится загрузить
+# без предварительной инициализации pygame
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
 
-    def update(self, *args):
-        self.image = self.image_arrow
-        
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+all_sprites = pygame.sprite.Group()
+sprite = pygame.sprite.Sprite(all_sprites)
+sprite.image = load_image("arrow.png")
+sprite.rect = sprite.image.get_rect()
+
+
+clock = pygame.time.Clock()
+fps = 144
+running = True
+sprite.rect.x = 5
+sprite.rect.y = 20
+pygame.mouse.set_visible(False)
+while running:
+    screen.fill(pygame.Color("black"))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEMOTION:
+            sprite.rect.x = event.pos[0] - 10
+            sprite.rect.y = event.pos[1] - 10
+    all_sprites.draw(screen)
+    pygame.display.flip()
+    clock.tick(fps)
+
+pygame.quit()
+
